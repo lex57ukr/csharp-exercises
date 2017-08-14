464a9efb2844a989845038061efd424a0cb80c80
Alex Akhtyrskiy <lex57ukr@gmail.com>
2017-08-14 12:14:49 -0600

    use better naming

--- sum-of-multiples/SumOfMultiples.cs
+++ sum-of-multiples/SumOfMultiples.cs
@@ -5,13 +5,13 @@ using System.Collections.Generic;
 
 public static class SumOfMultiples
 {
-    public static int To(IEnumerable<int> multiples, int max)
+    public static int To(IEnumerable<int> factors, int max)
     {
         IEnumerable<int> Multiples(int factor) => Enumerable
             .Range(1, max - 1)
             .Where(n => n % factor == 0);
 
-        return multiples
+        return factors
             .SelectMany(Multiples)
             .Distinct()
             .Sum();

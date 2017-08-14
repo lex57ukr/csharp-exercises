db7554160634878b325964c7106f446881637ee6
Alex Akhtyrskiy <lex57ukr@gmail.com>
2017-08-14 12:04:26 -0600

    add sum-of-multiples exercise

---
+++ sum-of-multiples/SumOfMultiples.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+public static class SumOfMultiples
+{
+    public static int To(IEnumerable<int> multiples, int max)
+    {
+        return multiples
+            .SelectMany(factor => MultiplesOf(factor, max))
+            .Distinct()
+            .Sum();
+    }
+
+    static IEnumerable<int> MultiplesOf(int factor, int max)
+    {
+        for ( int n = factor; n < max; n += factor )
+        {
+            yield return n;
+        }
+    }
+}

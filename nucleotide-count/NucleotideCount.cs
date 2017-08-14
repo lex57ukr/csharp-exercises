d42f0a86c0440a1d1687656b9cbf29b3ab8177e6
Alex Akhtyrskiy <lex57ukr@gmail.com>
2017-08-14 15:10:49 -0600

    use the functional approach with immutable data

--- nucleotide-count/NucleotideCount.cs
+++ nucleotide-count/NucleotideCount.cs
@@ -1,29 +1,31 @@
 using System;
 using System.Linq;
 using System.Collections.Generic;
+using System.Collections.Immutable;
+
 
 public class DNA
 {
+    const string Strand = "ATCG";
+
     public DNA(string sequence)
     {
-        foreach (char c in sequence)
-        {
-            if (this.NucleotideCounts.ContainsKey(c))
-            {
-                this.NucleotideCounts[c] += 1;
-            }
-        }
+        KeyValuePair<char, int> IndexedCount(char nucleotide)
+            => new KeyValuePair<char, int>(
+                nucleotide,
+                sequence.Count(c => c == nucleotide)
+            );
+
+        this.NucleotideCounts = Strand
+            .AsParallel()
+            .Select(IndexedCount)
+            .ToImmutableDictionary();
     }
 
     public IDictionary<char, int> NucleotideCounts
     {
         get;
-    } = new Dictionary<char, int> {
-        {'A', 0},
-        {'T', 0},
-        {'C', 0},
-        {'G', 0}
-    };
+    }
 
     public int Count(char nucleotide)
     {

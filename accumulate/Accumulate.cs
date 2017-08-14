4217baa1cec43b447be9152fe0624f375e7a444e
Alex Akhtyrskiy <lex57ukr@gmail.com>
2017-08-14 15:57:08 -0600

    add accumulate exercise

---
+++ accumulate/Accumulate.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+
+public static class AccumulateExtensions
+{
+    public static IEnumerable<TResult> Accumulate<TSource, TResult>(
+        this IEnumerable<TSource> collection,
+        Func<TSource, TResult> func
+    )
+    {
+        foreach (var item in collection)
+        {
+            yield return func(item);
+        }
+    }
+}

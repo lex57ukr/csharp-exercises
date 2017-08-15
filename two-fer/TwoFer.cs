f7c80cabf8aa72a733221bd3a5cbd221a56e3565
Alex Akhtyrskiy <lex57ukr@gmail.com>
2017-08-15 12:50:38 -0600

    add two-fer exercise

---
+++ two-fer/TwoFer.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+  public static class TwoFer
+  {
+      public static string GetResponse(string input = null)
+      {
+          var name = input ?? "you";
+          return $"One for {name}, one for me.";
+      }
+  }

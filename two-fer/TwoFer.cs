b578c42f4e16b1ff17a24d4a6e4ac323e2d831a4
Alex Akhtyrskiy <lex57ukr@gmail.com>
2017-08-15 12:51:03 -0600

    fix formatting

--- two-fer/TwoFer.cs
+++ two-fer/TwoFer.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 
-  public static class TwoFer
-  {
-      public static string GetResponse(string input = null)
-      {
-          var name = input ?? "you";
-          return $"One for {name}, one for me.";
-      }
-  }
+
+public static class TwoFer
+{
+    public static string GetResponse(string input = null)
+    {
+        var name = input ?? "you";
+        return $"One for {name}, one for me.";
+    }
+}

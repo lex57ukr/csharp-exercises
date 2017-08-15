9c784150eee37a332459b44a008a4081360f5b07
Alex Akhtyrskiy <lex57ukr@gmail.com>
2017-08-15 12:56:14 -0600

    use the expression syntax

--- two-fer/TwoFer.cs
+++ two-fer/TwoFer.cs
@@ -4,8 +4,5 @@
 public static class TwoFer
 {
     public static string GetResponse(string input = null)
-    {
-        var name = input ?? "you";
-        return $"One for {name}, one for me.";
-    }
+        => $"One for {input ?? "you"}, one for me.";
 }

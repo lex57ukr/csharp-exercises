aa1ed3f81757b59b5f1d3b12707b483b4af380d5
Alex Akhtyrskiy <lex57ukr@gmail.com>
2017-08-16 10:59:05 -0600

    add collatz-conjecture exercise

---
+++ collatz-conjecture/CollatzConjecture.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+public static class CollatzConjecture
+{
+    public static int GetSteps(int input)
+    {
+        if (input <= 0)
+        {
+            throw new ArgumentException();
+        }
+
+        return CountSteps(input, step: 0);
+    }
+
+    static int CountSteps(int n, int step)
+    {
+        if (1 == n)
+        {
+            return step;
+        }
+
+        return CountSteps(Next(n), step + 1);
+    }
+
+    static int Next(int n) => n % 2 == 0
+        ? n / 2
+        : 3 * n + 1;
+}

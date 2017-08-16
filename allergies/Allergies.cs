2f96a95adae206103d6f378a1b7d3594733a94bb
Alex Akhtyrskiy <lex57ukr@gmail.com>
2017-08-16 15:42:52 -0600

    use better naming

--- allergies/Allergies.cs
+++ allergies/Allergies.cs
@@ -6,15 +6,15 @@ using System.Collections.Immutable;
 
 public class Allergies
 {
-    readonly (int score, string allergen)[] Allergens = {
-        (score:   1, allergen: "eggs"),
-        (score:   2, allergen: "peanuts"),
-        (score:   4, allergen: "shellfish"),
-        (score:   8, allergen: "strawberries"),
-        (score:  16, allergen: "tomatoes"),
-        (score:  32, allergen: "chocolate"),
-        (score:  64, allergen: "pollen"),
-        (score: 128, allergen: "cats"),
+    readonly (int score, string name)[] Allergens = {
+        (score:   1, name: "eggs"),
+        (score:   2, name: "peanuts"),
+        (score:   4, name: "shellfish"),
+        (score:   8, name: "strawberries"),
+        (score:  16, name: "tomatoes"),
+        (score:  32, name: "chocolate"),
+        (score:  64, name: "pollen"),
+        (score: 128, name: "cats"),
     };
 
     ImmutableList<string> _allergies;
@@ -23,7 +23,7 @@ public class Allergies
     {
         _allergies = Allergens
             .Where(a => (mask & a.score) == a.score)
-            .Select(a => a.allergen)
+            .Select(a => a.name)
             .ToImmutableList();
     }
 

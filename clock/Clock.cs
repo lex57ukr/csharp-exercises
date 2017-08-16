adbddd00a8dae49069a27a844e766492def1a188
Alex Akhtyrskiy <lex57ukr@gmail.com>
2017-08-16 12:09:56 -0600

    add clock exercise

---
+++ clock/Clock.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+
+
+public class Clock
+    : IEquatable<Clock>
+{
+    const int HoursPerDay    = 24;
+    const int MinutesPerHour = 60;
+    const int MinutesPerDay  = HoursPerDay * MinutesPerHour;
+
+    public int Hours   => this.TotalMinutes / MinutesPerHour;
+
+    public int Minutes => this.TotalMinutes % MinutesPerHour;
+
+    public int TotalMinutes
+    {
+        get;
+    }
+
+    Clock(int totalMinutes)
+    {
+        int CappedMinutes()
+            => totalMinutes % MinutesPerDay;
+
+        int WrappedMinutes()
+            => MinutesPerDay - Math.Abs(CappedMinutes());
+
+        this.TotalMinutes = totalMinutes > 0
+            ? CappedMinutes()
+            : WrappedMinutes();
+    }
+
+    public Clock(int hours, int minutes = 0)
+        : this(MinutesPerHour * hours + minutes)
+    {
+    }
+
+    public Clock Add(int minutesToAdd)
+    {
+        return new Clock(this.TotalMinutes + minutesToAdd);
+    }
+
+    public Clock Subtract(int minutesToSubtract)
+    {
+        return new Clock(this.TotalMinutes - minutesToSubtract);
+    }
+
+    public override string ToString()
+        => $"{this.Hours:00}:{this.Minutes:00}";
+
+    public bool Equals(Clock other)
+        => null != other && this.TotalMinutes == other.TotalMinutes;
+}

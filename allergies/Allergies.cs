298a88679cf5cc1f24203ad8cf045417301e9530
Alex Akhtyrskiy <lex57ukr@gmail.com>
2017-08-16 15:18:15 -0600

    add allergies exercise

---
+++ allergies/Allergies.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+
+
+public class Allergies
+{
+    readonly (int score, string allergen)[] Allergens = {
+        (score:   1, allergen: "eggs"),
+        (score:   2, allergen: "peanuts"),
+        (score:   4, allergen: "shellfish"),
+        (score:   8, allergen: "strawberries"),
+        (score:  16, allergen: "tomatoes"),
+        (score:  32, allergen: "chocolate"),
+        (score:  64, allergen: "pollen"),
+        (score: 128, allergen: "cats"),
+    };
+
+    IImmutableList<string> _allergies;
+
+    public Allergies(int mask)
+    {
+        _allergies = Allergens
+            .Where(a => (mask & a.score) == a.score)
+            .Select(a => a.allergen)
+            .ToImmutableList();
+    }
+
+    public bool IsAllergicTo(string allergy)
+        => _allergies.Contains(allergy);
+
+    public IList<string> List()
+        => _allergies as IList<string>;
+}

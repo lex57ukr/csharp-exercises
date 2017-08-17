22ca01b0899722e7e0c2fec3c1066b3133f90ae8
Alex Akhtyrskiy <lex57ukr@gmail.com>
2017-08-16 21:25:51 -0600

    calculate both Min and Max once in a lazy manner

--- saddle-points/SaddlePoints.cs
+++ saddle-points/SaddlePoints.cs
@@ -6,36 +6,55 @@ using System.Collections.Generic;
 public class SaddlePoints
 {
     readonly int[,] _values;
+    readonly Lazy<int[]> _maxInRows;
+    readonly Lazy<int[]> _minInColumns;
 
-    public int Width => _values.GetLength(0);
-
-    public int Height => _values.GetLength(1);
+    int Width => _values.GetLength(0);
+    int Height => _values.GetLength(1);
 
     public SaddlePoints(int[,] values)
-        => _values = values;
+    {
+        _values = values;
+
+        _maxInRows = new Lazy<int[]>(
+            () => Enumerable
+                .Range(0, this.Width)
+                .Select(MaxForRow)
+                .ToArray(),
+            isThreadSafe: true
+        );
+
+        _minInColumns = new Lazy<int[]>(
+            () => Enumerable
+                .Range(0, this.Height)
+                .Select(MinForColumn)
+                .ToArray(),
+            isThreadSafe: true
+        );
+    }
 
     public IEnumerable<Tuple<int, int>> Calculate()
         => Indices().Where(IsSaddlePoint);
 
-    IEnumerable<Tuple<int, int>> Indices()
-    {
-        for (int x = 0; x < this.Width; ++x)
-        {
-            for (int y = 0; y < this.Height; ++y)
-            {
-                yield return Tuple.Create(x, y);
-            }
-        }
-    }
+    IEnumerable<Tuple<int, int>> Indices() =>
+        from x in Enumerable.Range(0, this.Width)
+        from y in Enumerable.Range(0, this.Height)
+        select Tuple.Create(x, y);
 
     bool IsSaddlePoint(Tuple<int, int> index)
     {
         int x = index.Item1, y = index.Item2;
         int value = _values[x, y];
 
-        return value == MaxForRow(x)
-            && value == MinForColumn(y);
+        return IsMaxForRow(value, x)
+            && IsMinForColumn(value, y);
     }
+
+    bool IsMaxForRow(int value, int row)
+        => value == _maxInRows.Value[row];
+
+    bool IsMinForColumn(int value, int column)
+        => value == _minInColumns.Value[column];
 
     int MaxForRow(int row) => Enumerable
         .Range(0, this.Height)

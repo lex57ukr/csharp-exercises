9873adae9681413c86fd4e0b27485a928e2238f8
Alex Akhtyrskiy <lex57ukr@gmail.com>
2017-08-27 23:57:58 -0600

    add gigasecond exercise

---
+++ gigasecond/Gigasecond.cs
@@ -0,0 +1,11 @@
+using System;
+
+
+public static class Gigasecond
+{
+    const long Seconds = 1_000_000_000L;
+    const long Ticks = Seconds * TimeSpan.TicksPerSecond;
+
+    public static DateTime Add(DateTime birthDate)
+        => birthDate.AddTicks(Ticks);
+}

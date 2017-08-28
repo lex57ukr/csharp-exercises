e3f6cb4f16aac72973d65628ff1296b9b58054bb
Alex Akhtyrskiy <lex57ukr@gmail.com>
2017-08-27 23:58:31 -0600

    align consts

--- gigasecond/Gigasecond.cs
+++ gigasecond/Gigasecond.cs
@@ -4,7 +4,7 @@ using System;
 public static class Gigasecond
 {
     const long Seconds = 1_000_000_000L;
-    const long Ticks = Seconds * TimeSpan.TicksPerSecond;
+    const long Ticks   = Seconds * TimeSpan.TicksPerSecond;
 
     public static DateTime Add(DateTime birthDate)
         => birthDate.AddTicks(Ticks);

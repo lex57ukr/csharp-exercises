ca3800bf033272279d15eb8e9c2f72a29da4bc91
Alex Akhtyrskiy <lex57ukr@gmail.com>
2017-08-31 01:51:09 -0600

    use Math.Pow function

--- grains/Grains.cs
+++ grains/Grains.cs
@@ -1,19 +1,11 @@
 ﻿using System;
-using System.Linq;
-using System.Collections.Generic;
 
 
 public static class Grains
 {
-    const int MaxSquares = 64;
-
     public static ulong Square(int n)
-        => TwoInPower(n - 1);
+        => (ulong) Math.Pow(2, n - 1);
 
     public static ulong Total()
-        => TwoInPower(MaxSquares) - 1;
-
-    static ulong TwoInPower(int n) => Enumerable
-        .Range(1, n)
-        .Aggregate((ulong) 1, (acc, _) => acc * 2);
+        => 18_446_744_073_709_551_615L;
 }

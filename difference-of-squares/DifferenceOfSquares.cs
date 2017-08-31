32c7e03796b3d07a486344b8b6cf3aee2e8852f6
Alex Akhtyrskiy <lex57ukr@gmail.com>
2017-08-31 00:34:11 -0600

    add difference-of-squares exercise

---
+++ difference-of-squares/DifferenceOfSquares.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+public static class Squares
+{
+    public static int SquareOfSums(int max) => max
+        .NaturalNumbers()
+        .Sum()
+        .Square();
+
+    public static int SumOfSquares(int max) => max
+        .NaturalNumbers()
+        .Select(Square)
+        .Sum();
+
+    public static int DifferenceOfSquares(int max)
+        => SquareOfSums(max) - SumOfSquares(max);
+
+    static IEnumerable<int> NaturalNumbers(this int max)
+        => Enumerable.Range(start: 1, count: max);
+
+    static int Square(this int value)
+        => value * value;
+}

548d6541c2a11b81a69c42c12c7076a67025235b
Alex Akhtyrskiy <lex57ukr@gmail.com>
2017-08-31 01:35:44 -0600

    add the grains exercise

---
+++ grains/Grains.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+public static class Grains
+{
+    const int MaxSquares = 64;
+
+    public static ulong Square(int n)
+        => TwoInPower(n - 1);
+
+    public static ulong Total()
+        => TwoInPower(MaxSquares) - 1;
+
+    static ulong TwoInPower(int n) => Enumerable
+        .Range(1, n)
+        .Aggregate((ulong) 1, (acc, _) => acc * 2);
+}

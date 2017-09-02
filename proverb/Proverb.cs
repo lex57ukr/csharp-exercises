cc07b869dd5c9ddc9c4efaec9aeee2c5ed1a7221
Alex Akhtyrskiy <lex57ukr@gmail.com>
2017-09-01 23:56:59 -0600

    add fluent syntax with deferred evaluation

--- proverb/Proverb.cs
+++ proverb/Proverb.cs
@@ -5,19 +5,35 @@ using System.Collections.Generic;
 
 public static class Proverb
 {
-    static readonly string[] Lines = {
-        "For want of a nail the shoe was lost.",
-        "For want of a shoe the horse was lost.",
-        "For want of a horse the rider was lost.",
-        "For want of a rider the message was lost.",
-        "For want of a message the battle was lost.",
-        "For want of a battle the kingdom was lost.",
-        "And all for the want of a horseshoe nail.",
+    static readonly Func<string>[] Lines = {
+        "nail".Caused("shoe").ToBe().Lost(),
+        "shoe".Caused("horse").ToBe().Lost(),
+        "horse".Caused("rider").ToBe().Lost(),
+        "rider".Caused("message").ToBe().Lost(),
+        "message".Caused("battle").ToBe().Lost(),
+        "battle".Caused("kingdom").ToBe().Lost(),
+        "And all for the want of a horseshoe nail.".ToBe(),
     };
 
+    public static IEnumerable<int> Indices
+        => Enumerable.Range(1, Lines.Length);
+
     public static string Line(int line)
-        => Lines[line - 1];
+        => Lines[line - 1]();
 
     public static string AllLines()
-        => String.Join("\n", Lines);
+        => String.Join("\n", Indices.Select(Line));
+
+    static (string, string) Caused(
+        this string subject, string item
+    ) => (subject, item);
+
+    static Func<T> ToBe<T>(this T value)
+        => (() => value);
+
+    static Func<string> Lost(this Func<(string, string)> context)
+        => (() => context().Lament());
+
+    static string Lament(this (string subject, string item) x)
+        => $"For want of a {x.subject} the {x.item} was lost.";
 }

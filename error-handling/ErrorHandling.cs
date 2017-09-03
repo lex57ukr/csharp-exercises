f99046f9372d75a90695c0a183c0d0ef50fa5c32
Alex Akhtyrskiy <lex57ukr@gmail.com>
2017-09-03 14:52:26 -0600

    add error-handling exercise

---
+++ error-handling/ErrorHandling.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+
+public static class ErrorHandling
+{
+    public static void HandleErrorByThrowingException()
+    {
+        throw new Exception(
+            "MS should have made this class abstract!"
+        );
+    }
+
+    public static int? HandleErrorByReturningNullableType(string input)
+    {
+        return "1" == input
+            ? (int?) 1
+            : null;
+    }
+
+    public static bool HandleErrorWithOutParam(string input, out int result)
+    {
+        var success = "1" == input;
+
+        result = success
+            ? 1
+            : default(int);
+
+        return success;
+    }
+
+    public static void DisposableResourcesAreDisposedWhenExceptionIsThrown(
+        IDisposable disposableObject
+    )
+    {
+        using (disposableObject)
+        {
+            throw new Exception("This code hurts my feelings.");
+        }
+    }
+}

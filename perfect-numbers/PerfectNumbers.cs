9a20a017281899095cc9e2e72518856f80a6a231
Alex Akhtyrskiy <lex57ukr@gmail.com>
2017-09-03 16:57:13 -0600

    add perfect-numbers exercise

---
+++ perfect-numbers/PerfectNumbers.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+public enum Classification
+{
+    Perfect,
+    Abundant,
+    Deficient
+}
+
+
+public static class PerfectNumbers
+{
+    static readonly (
+        Func<int, int, bool> matcher, Classification kind
+    )[] Classes = {
+        ((a, n) => a == n, Classification.Perfect),
+        ((a, n) => a > n, Classification.Abundant),
+        ((a, n) => a < n, Classification.Deficient),
+    };
+
+    public static Classification Classify(int number)
+        => number.AliquotSum().ClassOf(number);
+
+    static int AliquotSum(this int number)
+        => number.Factors().Sum();
+
+    static IEnumerable<int> Factors(this int number) => Enumerable
+        .Range(1, count: number - 1)
+        .Where(n => number % n == 0);
+
+    static Classification ClassOf(this int aliquotSum, int number) => Classes
+        .Where(c => c.matcher(aliquotSum, number))
+        .Select(c => c.kind)
+        .First();
+}

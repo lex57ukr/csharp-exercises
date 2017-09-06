a07d269addfc3de230c2eef0e03b2d7436e15e63
Alex Akhtyrskiy <lex57ukr@gmail.com>
2017-09-05 23:18:49 -0600

    add secret-handshake exersice

---
+++ secret-handshake/SecretHandshake.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+
+public static class SecretHandshake
+{
+    const string Reverse = ":reverse";
+
+    static readonly (int mask, string code)[] Codes = {
+        (mask: 1, code:  "wink"),
+        (mask: 2, code:  "double blink"),
+        (mask: 4, code:  "close your eyes"),
+        (mask: 8, code:  "jump"),
+        (mask: 16, code: Reverse),
+    };
+
+    public static string[] Commands(int commandValue) => Codes
+        .Aggregate(
+            ImmutableList<string>.Empty,
+            (acc, ctl) => (ctl.mask & commandValue) == ctl.mask
+                ? acc.Apply(ctl.code)
+                : acc
+        ).ToArray();
+
+    static ImmutableList<string> Apply(
+        this ImmutableList<string> acc,
+        string code
+    ) => code == Reverse ? acc.Reverse() : acc.Add(code);
+}

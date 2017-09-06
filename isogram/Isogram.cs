c5d4d81ecbea300a3d717e55c64b775548517f3d
Alex Akhtyrskiy <lex57ukr@gmail.com>
2017-09-05 22:42:52 -0600

    add isogram exercise

---
+++ isogram/Isogram.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+public static class Isogram
+{
+    public static bool IsIsogram(string word) => word
+        .Where(char.IsLetter)
+        .Select(char.ToLower)
+        .GroupBy(c => c)
+        .FirstOrDefault(g => g.Count() > 1) == null;
+}

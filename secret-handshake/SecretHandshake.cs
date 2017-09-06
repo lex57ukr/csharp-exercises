b651df26c53b88128eca21f7a3dd9367bde25be1
Alex Akhtyrskiy <lex57ukr@gmail.com>
2017-09-05 23:35:04 -0600

    do not use an aggregator

--- secret-handshake/SecretHandshake.cs
+++ secret-handshake/SecretHandshake.cs
@@ -1,31 +1,33 @@
 ﻿using System;
 using System.Linq;
 using System.Collections.Generic;
-using System.Collections.Immutable;
 
 
 public static class SecretHandshake
 {
-    const string Reverse = ":reverse";
+    const int CtlReverse = 16;
 
     static readonly (int mask, string code)[] Codes = {
         (mask: 1, code:  "wink"),
         (mask: 2, code:  "double blink"),
         (mask: 4, code:  "close your eyes"),
         (mask: 8, code:  "jump"),
-        (mask: 16, code: Reverse),
     };
 
-    public static string[] Commands(int commandValue) => Codes
-        .Aggregate(
-            ImmutableList<string>.Empty,
-            (acc, ctl) => (ctl.mask & commandValue) == ctl.mask
-                ? acc.Apply(ctl.code)
-                : acc
-        ).ToArray();
+    public static string[] Commands(int commandValue)
+    {
+        bool IsOn(int mask)
+            => (mask & commandValue) == mask;
 
-    static ImmutableList<string> Apply(
-        this ImmutableList<string> acc,
-        string code
-    ) => code == Reverse ? acc.Reverse() : acc.Add(code);
+        return Codes
+            .Where(ctl => IsOn(ctl.mask))
+            .Select(ctl => ctl.code)
+            .ReverseIfTrue(IsOn(CtlReverse))
+            .ToArray();
+    }
+
+    static IEnumerable<string> ReverseIfTrue(
+        this IEnumerable<string> source,
+        bool cond
+    ) => cond ? source.Reverse() : source;
 }

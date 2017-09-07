4c793fb925a4ebcf922e479e943b6fe74ce5fb4b
Alex Akhtyrskiy <lex57ukr@gmail.com>
2017-09-07 00:17:44 -0600

    add acronym exercise

---
+++ acronym/Acronym.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+public static class Acronym
+{
+    static readonly char[] Separators = {' ', '-', ','};
+
+    public static string Abbreviate(string phrase)
+        => string.Concat(phrase.TitleLetters());
+
+    static IEnumerable<char> TitleLetters(this string phrase) => phrase
+        .Split(Separators, StringSplitOptions.RemoveEmptyEntries)
+        .Select(w => w[0])
+        .Select(char.ToUpper);
+}

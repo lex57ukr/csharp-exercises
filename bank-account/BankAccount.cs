ce440044e25f073ccfef32d35fcb237102d8445f
Alex Akhtyrskiy <lex57ukr@gmail.com>
2017-09-06 23:24:03 -0600

    add bank-account exercise

---
+++ bank-account/BankAccount.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+
+public class BankAccount
+{
+    readonly object _monitor = new object();
+    float _balance;
+    bool _open;
+
+    public float Balance
+        => SyncChecked(() => _balance);
+
+    public void Open()
+        => Sync(() => _open = true);
+
+    public void Close()
+        => Sync(() => _open = false);
+
+    public void UpdateBalance(float change)
+        => SyncChecked(() => _balance += change);
+
+    T Sync<T>(Func<T> method)
+    {
+        lock (_monitor)
+        {
+            return method();
+        }
+    }
+
+    T SyncChecked<T>(Func<T> method)
+        => Sync<T>(() => Checked(method));
+
+    T Checked<T>(Func<T> method)
+    {
+        if ( ! _open)
+        {
+            throw new InvalidOperationException("The account is closed.");
+        }
+
+        return method();
+    }
+}

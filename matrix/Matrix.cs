30e8af87e2ba48703b6b06fb3afb0f3747f6f40d
Alex Akhtyrskiy <lex57ukr@gmail.com>
2017-09-07 15:21:25 -0600

    use consistent prefix operators

--- matrix/Matrix.cs
+++ matrix/Matrix.cs
@@ -47,7 +47,7 @@ static class EnumerableExtensions
             .ToArray();
 
         var res = new T[data.Length, data.Max(x => x.Length)];
-        for (var i = 0; i < data.Length; i++)
+        for (var i = 0; i < data.Length; ++i)
         {
             for (var j = 0; j < data[i].Length; ++j)
             {

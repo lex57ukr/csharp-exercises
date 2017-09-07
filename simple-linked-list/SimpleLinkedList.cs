ee1debdfcba11282300489ee4d2de46fd4533731
Alex Akhtyrskiy <lex57ukr@gmail.com>
2017-09-07 01:21:13 -0600

    add simple-linked-list exercise

---
+++ simple-linked-list/SimpleLinkedList.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class SimpleLinkedList<T>
+    : IEnumerable<T>
+{
+    public SimpleLinkedList(T value)
+        => this.Value = value;
+
+    public SimpleLinkedList(IEnumerable<T> values)
+        : this(values.First())
+    {
+        foreach (var value in values.Skip(1))
+        {
+            Add(value);
+        }
+    }
+
+    public T Value 
+    { 
+        get;
+        private set;
+    }
+
+    public SimpleLinkedList<T> Next
+    { 
+        get;
+        private set;
+    }
+
+    public SimpleLinkedList<T> Add(T value)
+    {
+        if (this.Next == null)
+        {
+            this.Next = new SimpleLinkedList<T>(value);
+        }
+        else
+        {
+            this.Next.Add(value);
+        }
+
+        return this;
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        for (var curr = this; curr != null; curr = curr.Next)
+        {
+            yield return curr.Value;
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+        => GetEnumerator();
+}

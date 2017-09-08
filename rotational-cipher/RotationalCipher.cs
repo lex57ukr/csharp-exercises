4b903b796f3e9e7bfddc6a66c25641c94dfb3ab9
Alex Akhtyrskiy <lex57ukr@gmail.com>
2017-09-07 21:14:04 -0600

    make it a bit less functional

--- rotational-cipher/RotationalCipher.cs
+++ rotational-cipher/RotationalCipher.cs
@@ -5,14 +5,6 @@ using System.Collections.Generic;
 
 public static class RotationalCipher
 {
-    static readonly (
-        Func<char, bool> eval,
-        Func<char, int, char> rot
-    )[] Rotators = {
-        (eval: IsUpper, rot: (c, shift) => c.Rot('A', shift)),
-        (eval: IsLower, rot: (c, shift) => c.Rot('a', shift)),
-    };
-
     public static string Rotate(string text, int shift)
         => string.Concat(text.Rot(shift));
 
@@ -20,22 +12,24 @@ public static class RotationalCipher
         => text.Select(c => c.Rot(shift));
 
     static char Rot(this char c, int shift)
-        => Rotators
-        .Where(x => x.eval(c))
-        .Select(x => x.rot(c, shift))
-        .DefaultIfEmpty(c)
-        .First();
+    {
+        if ('A' <= c && c <= 'Z')
+        {
+            return c.Rot('A', shift);
+        }
+
+        if ('a' <= c && c <= 'z')
+        {
+            return c.Rot('a', shift);
+        }
+
+        return c;
+    }
 
     static char Rot(this char x, char a, int shift)
         => Rot((int) x, (int) a, shift);
 
     static char Rot(int x, int a, int shift)
         => (char) (a + (x - a + shift) % 26);
-
-    static bool IsUpper(this char c)
-        => 'A' <= c && c <= 'Z';
-
-    static bool IsLower(this char c)
-        => 'a' <= c && c <= 'z';
 }
 

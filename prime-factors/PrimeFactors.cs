402e6a30d9ce658e923f0a00d272c2eed56e2509
Alex Akhtyrskiy <lex57ukr@gmail.com>
2017-09-30 13:58:15 -0600

    implement prime-factors exercise

--- prime-factors/PrimeFactors.cs
+++ prime-factors/PrimeFactors.cs
@@ -6,7 +6,24 @@ using System.Collections.Generic;
 public static class PrimeFactors
 {
     public static int[] Factors(long number)
+        => EnumFactors(number).ToArray();
+
+    private static IEnumerable<int> EnumFactors(long number)
     {
-        throw new NotImplementedException();
+        var n = number;
+        var factor = 2;
+
+        while (n != 1)
+        {
+            if (n % factor != 0)
+            {
+                factor++;
+            }
+            else
+            {
+                n /= factor;
+                yield return factor;
+            }
+        }
     }
 }

df5489ff73bc990a2b1193a0d3e50a9544901b37
Alex Akhtyrskiy <lex57ukr@gmail.com>
2017-09-30 12:53:05 -0600

    add prime-factors exercise (TODO)

---
+++ prime-factors/PrimeFactors.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+public static class PrimeFactors
+{
+    public static int[] Factors(long number)
+    {
+        throw new NotImplementedException();
+    }
+}

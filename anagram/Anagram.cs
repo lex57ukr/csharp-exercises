d2a3e353f38e7fcb5cc0ad5a92069ef3acc7ae0e
Alex Akhtyrskiy <lex57ukr@gmail.com>
2017-09-30 12:09:25 -0600

    add anagram exercise

--- anagram/Anagram.cs
+++ anagram/Anagram.cs
@@ -1,15 +1,33 @@
 using System;
+using System.Linq;
+using System.Collections.Generic;
 
 
 public class Anagram
 {
+    private string BaseWord { get; }
+    private string Alphabet { get; }
+    private static IEqualityComparer<string> Comparer
+        => StringComparer.OrdinalIgnoreCase;
+
     public Anagram(string baseWord)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        this.BaseWord = baseWord;
+        this.Alphabet = GetAlphabet(baseWord);
     }
 
     public string[] Anagrams(string[] potentialMatches)
+        => potentialMatches
+            .Distinct(Comparer)
+            .Where(IsAnagram)
+            .ToArray();
+
+    private static string GetAlphabet(string word)
+        => string.Concat(word.Select(char.ToLower).OrderBy(c => c));
+
+    private bool IsAnagram(string word)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return ! Comparer.Equals(this.BaseWord, word)
+            && this.Alphabet == GetAlphabet(word);
     }
 }

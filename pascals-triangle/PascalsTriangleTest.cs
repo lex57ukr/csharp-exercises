12113e23bee588b906aebd8e2f183eb409a3ec8b
Alex Akhtyrskiy <lex57ukr@gmail.com>
2017-10-14 14:35:22 -0600

    Add the pascal-triangle exercise (TODO)

---
+++ pascals-triangle/PascalsTriangleTest.cs
@@ -0,0 +1,48 @@
+// This file was auto-generated based on version 1.0.0 of the canonical data.
+
+using Xunit;
+using System;
+
+
+public class PascalsTriangleTest
+{
+    [Fact]
+    public void Zero_rows()
+    {
+        Assert.Empty(PascalsTriangle.Calculate(0));
+    }
+
+    [Fact(Skip = "Remove to run test")]
+    public void Single_row()
+    {
+        var expected = new[] { new[] { 1 } };
+        Assert.Equal(expected, PascalsTriangle.Calculate(1));
+    }
+
+    [Fact(Skip = "Remove to run test")]
+    public void Two_rows()
+    {
+        var expected = new[] { new[] { 1 }, new[] { 1, 1 } };
+        Assert.Equal(expected, PascalsTriangle.Calculate(2));
+    }
+
+    [Fact(Skip = "Remove to run test")]
+    public void Three_rows()
+    {
+        var expected = new[] { new[] { 1 }, new[] { 1, 1 }, new[] { 1, 2, 1 } };
+        Assert.Equal(expected, PascalsTriangle.Calculate(3));
+    }
+
+    [Fact(Skip = "Remove to run test")]
+    public void Four_rows()
+    {
+        var expected = new[] { new[] { 1 }, new[] { 1, 1 }, new[] { 1, 2, 1 }, new[] { 1, 3, 3, 1 } };
+        Assert.Equal(expected, PascalsTriangle.Calculate(4));
+    }
+
+    [Fact(Skip = "Remove to run test")]
+    public void Negative_rows()
+    {
+        Assert.Throws<ArgumentOutOfRangeException>(() => PascalsTriangle.Calculate(-1));
+    }
+}

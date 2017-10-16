ba7f9c625c93fbf65874e464b3a180230ce6bcbc
Alex Akhtyrskiy <lex57ukr@gmail.com>
2017-10-16 17:31:27 -0600

    Implement pascals-triangle

--- pascals-triangle/PascalsTriangle.cs
+++ pascals-triangle/PascalsTriangle.cs
@@ -1,11 +1,24 @@
 ﻿using System;
+using System.Linq;
 using System.Collections.Generic;
+using System.Collections.Immutable;
+using static System.Linq.Enumerable;
 
 
 public static class PascalsTriangle
 {
     public static IEnumerable<IEnumerable<int>> Calculate(int rows)
-    {
-        throw new NotImplementedException();
-    }
+        => Range(0, rows).Aggregate(
+            ImmutableList<IEnumerable<int>>.Empty,
+            (acc, i) => acc.Add(Row(i))
+        );
+
+    private static IEnumerable<int> Row(int n)
+        => Range(0, n).Aggregate(
+            ImmutableList<int>.Empty.Add(1),
+            (acc, i) => acc.Add(Next(acc.Last(), i, n))
+        );
+
+    private static int Next(int prev, int i, int row)
+        => prev * (row - i) / (i + 1);
 }
--- pascals-triangle/PascalsTriangleTest.cs
+++ pascals-triangle/PascalsTriangleTest.cs
@@ -12,35 +12,35 @@ public class PascalsTriangleTest
         Assert.Empty(PascalsTriangle.Calculate(0));
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Single_row()
     {
         var expected = new[] { new[] { 1 } };
         Assert.Equal(expected, PascalsTriangle.Calculate(1));
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Two_rows()
     {
         var expected = new[] { new[] { 1 }, new[] { 1, 1 } };
         Assert.Equal(expected, PascalsTriangle.Calculate(2));
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Three_rows()
     {
         var expected = new[] { new[] { 1 }, new[] { 1, 1 }, new[] { 1, 2, 1 } };
         Assert.Equal(expected, PascalsTriangle.Calculate(3));
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Four_rows()
     {
         var expected = new[] { new[] { 1 }, new[] { 1, 1 }, new[] { 1, 2, 1 }, new[] { 1, 3, 3, 1 } };
         Assert.Equal(expected, PascalsTriangle.Calculate(4));
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Negative_rows()
     {
         Assert.Throws<ArgumentOutOfRangeException>(() => PascalsTriangle.Calculate(-1));

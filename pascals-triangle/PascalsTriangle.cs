e82949943ad56267ca33e56e96f8f5bebb3cb18d
Alexander Akhtyrskiy <lex57ukr@gmail.com>
2017-10-21 14:42:13 -0600

    use stack for faster prev number lookups

--- pascals-triangle/PascalsTriangle.cs
+++ pascals-triangle/PascalsTriangle.cs
@@ -15,8 +15,8 @@ public static class PascalsTriangle
 
     private static IEnumerable<int> Row(int n)
         => Range(0, n).Aggregate(
-            ImmutableList<int>.Empty.Add(1),
-            (acc, i) => acc.Add(Next(acc.Last(), i, n))
+            ImmutableStack<int>.Empty.Push(1),
+            (acc, i) => acc.Push(Next(acc.Peek(), i, n))
         );
 
     private static int Next(int prev, int i, int row)

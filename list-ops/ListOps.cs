abed3e64aad2610cd2f440fd8ea989d0523d86a4
Alexander Akhtyrskiy <lex57ukr@gmail.com>
2017-10-21 16:07:38 -0600

    implement list-ops exercize

--- list-ops/ListOps.cs
+++ list-ops/ListOps.cs
@@ -1,47 +1,58 @@
 ﻿using System;
-using System.Linq;
 using System.Collections.Generic;
 
 
 public static class ListOps
 {
     public static int Length<T>(List<T> input)
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
+        => input.Count;
 
     public static List<T> Reverse<T>(List<T> input)
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
+        => Foldr((x, acc) => Add(acc, x), new List<T>(), input);
 
     public static List<TOut> Map<TIn, TOut>(Func<TIn, TOut> map, List<TIn> input)
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
+        => Foldl((acc, x) => Add(acc, map(x)), new List<TOut>(), input);
 
     public static List<T> Filter<T>(Func<T, bool> predicate, List<T> input)
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
+        => Foldl((acc, x) => AddIf(predicate, acc, x), new List<T>(), input);
 
     public static TOut Foldl<TIn, TOut>(Func<TOut, TIn, TOut> func, TOut start, List<TIn> input)
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
+        => Fold(func, start, input);
 
     public static TOut Foldr<TIn, TOut>(Func<TIn, TOut, TOut> func, TOut start, List<TIn> input)
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
+        => Fold((acc, x) => func(x, acc), start, Reversed(input));
 
     public static List<T> Concat<T>(List<List<T>> input)
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
+        => Foldl(Append, new List<T>(), input);
 
     public static List<T> Append<T>(List<T> left, List<T> right)
+        => Foldl(Add, new List<T>(left), right);
+
+    private static TOut Fold<TIn, TOut>(Func<TOut, TIn, TOut> func, TOut start, IEnumerable<TIn> input)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        var acc = start;
+        foreach (var x in input)
+        {
+            acc = func(acc, x);
+        }
+
+        return acc;
     }
+
+    private static IEnumerable<T> Reversed<T>(List<T> list)
+    {
+        for (var i = Length(list) - 1; i >= 0; --i)
+        {
+            yield return list[i];
+        }
+    }
+
+    private static List<T> Add<T>(List<T> acc, T x)
+    {
+        acc.Add(x);
+        return acc;
+    }
+
+    private static List<T> AddIf<T>(Func<T, bool> predicate, List<T> acc, T x)
+        => predicate(x) ? Add(acc, x) : acc;
 }

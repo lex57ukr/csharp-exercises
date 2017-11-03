2ad8a8ba4f5f1896d8e341262cee91f4017d6905
Alex Akhtyrskiy <lex57ukr@gmail.com>
2017-11-02 21:53:25 -0600

    implement simple-cipher

--- simple-cipher/SimpleCipher.cs
+++ simple-cipher/SimpleCipher.cs
@@ -1,33 +1,64 @@
 using System;
+using System.Linq;
+using System.Collections.Generic;
+using static System.String;
+using static System.Linq.Enumerable;
 
 
 public class Cipher
 {
-    public Cipher()
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
+    private const int MinRandomKeyLength = 100;
+    private const string Alpha = "abcdefghijklmnopqrstuvwxyz";
+    private static readonly Random Rand = new Random();
 
-    public Cipher(string key)
+    private string _key;
+
+    public string Key
     {
-        throw new NotImplementedException("You need to implement this function.");
-    }
-    
-    public string Key 
-    {
-        get
+        get => _key;
+        private set
         {
-            throw new NotImplementedException("You need to implement this function.");
+            if ( ! IsValidKey(value))
+            {
+                throw new ArgumentException("Invalid key.");
+            }
+
+            _key = value;
         }
     }
 
+    public Cipher(string key)
+        => Key = key;
+
+    public Cipher()
+        => _key = CreateRandomKey();
+
     public string Encode(string plaintext)
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
+        => Transform(plaintext, EncodeDistance);
 
     public string Decode(string ciphertext)
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
+        => Transform(ciphertext, DecodeDistance);
+
+    private string Transform(string text, Func<char, char, int> distance)
+        => Concat(
+            Range(0, text.Length)
+                .Select(i => distance(text[i], this.Key[i]))
+                .Select(d => Alpha[d % Alpha.Length])
+        );
+
+    private static int EncodeDistance(char c, char k)
+        => Alpha.IndexOf(c) + Alpha.IndexOf(k);
+
+    private static int DecodeDistance(char c, char k)
+        => Alpha.IndexOf(c) - Alpha.IndexOf(k) + Alpha.Length;
+
+    private static string CreateRandomKey()
+        => Concat(
+            Range(0, MinRandomKeyLength)
+                .Select(i => Rand.Next(Alpha.Length))
+                .Select(i => Alpha[i])
+        );
+
+    private static bool IsValidKey(string key)
+        => ! IsNullOrEmpty(key) && key.All(c => Alpha.Contains(c));
 }

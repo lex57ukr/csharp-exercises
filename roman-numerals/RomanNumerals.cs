0d5cf54e8341cf28ca0c371f6a9eca7960c1b01e
Alex Akhtyrskiy <lex57ukr@gmail.com>
2017-11-06 16:52:35 -0700

    add roman-numerals exercise

---
+++ roman-numerals/RomanNumerals.cs
@@ -0,0 +1,10 @@
+using System;
+
+
+public static class RomanNumeralExtension
+{
+    public static string ToRoman(this int value)
+    {
+        throw new NotImplementedException("You need to implement this function.");
+    }
+}

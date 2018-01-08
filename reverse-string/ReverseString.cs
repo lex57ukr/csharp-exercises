b922b58657b857d429ce07758516f775b41bab14
Alexander Akhtyrskiy <lex57ukr@gmail.com>
2018-01-07 20:13:54 -0700

    use manual swap

--- reverse-string/ReverseString.cs
+++ reverse-string/ReverseString.cs
@@ -6,8 +6,18 @@ public static class ReverseString
     public static string Reverse(string input)
     {
         var chars = input.ToCharArray();
-        Array.Reverse(chars);
+        for (var i = 0; i < chars.Length / 2; ++i)
+        {
+            Swap(chars, i, chars.Length - i - 1);
+        }
 
         return new string(chars);
+    }
+
+    private static void Swap<T>(T[] array, int i, int j)
+    {
+        var t = array[i];
+        array[i] = array[j];
+        array[j] = t;
     }
 }

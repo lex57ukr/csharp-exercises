0d4a4069ed5c3fd68f2074c2c36ac57b0fd6c80b
Alexander Akhtyrskiy <lex57ukr@gmail.com>
2018-01-07 20:20:33 -0700

    use cleaner methods

--- reverse-string/ReverseString.cs
+++ reverse-string/ReverseString.cs
@@ -6,12 +6,17 @@ public static class ReverseString
     public static string Reverse(string input)
     {
         var chars = input.ToCharArray();
-        for (int i = 0, count = chars.Length / 2; i < count; ++i)
-        {
-            Swap(chars, i, chars.Length - i - 1);
-        }
+        Reverse(chars);
 
         return new string(chars);
+    }
+
+    private static void Reverse<T>(T[] array)
+    {
+        for (int i = 0, count = array.Length / 2; i < count; ++i)
+        {
+            Swap(array, i, chars.Length - i - 1);
+        }
     }
 
     private static void Swap<T>(T[] array, int i, int j)

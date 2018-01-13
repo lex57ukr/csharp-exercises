6458b6644702e881395e919a4cad5e25275a2f96
Alexander Akhtyrskiy <lex57ukr@gmail.com>
2018-01-13 16:25:26 -0700

    add isbn-verifier

---
+++ isbn-verifier/IsbnVerifier.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using static System.Linq.Enumerable;
+
+public static class IsbnVerifier
+{
+    private const int ZeroChar = 48;
+    private const string IsbnPattern = @"^\d(-?)\d{3}\1\d{5}\1[\dX]$";
+
+    public static bool IsValid(string number)
+        => IsIsbnPattern(number) && IsbnCheckSum(number) % 11 == 0;
+
+    private static int IsbnCheckSum(string number)
+        => SequenceDigits(number)
+        .Reverse()
+        .Select((n, i) => n * (i + 1))
+        .Sum();
+
+    private static bool IsIsbnPattern(string number)
+        => Regex.IsMatch(number, IsbnPattern);
+
+    private static IEnumerable<int> SequenceDigits(string number)
+        => number.Where(x => x != '-').Select(ToInt32);
+
+    private static int ToInt32(char c)
+        => c == 'X' ? 10 : Convert.ToInt32(c) - ZeroChar;
+}

89d3af365924e5841e7cb266649e793a8717bfb8
Alexander Akhtyrskiy <lex57ukr@gmail.com>
2018-01-13 15:34:52 -0700

    enforce method consistency

--- armstrong-numbers/ArmstrongNumbers.cs
+++ armstrong-numbers/ArmstrongNumbers.cs
@@ -17,8 +17,8 @@ public static class ArmstrongNumbers
     private static int CountDigits(int number)
         => (int) Math.Log10(number) + 1;
 
-    private static int Digit(int number, int index, int @base = 10)
-        => (number / Pow(@base, index)) % @base;
+    private static int Digit(int number, int index)
+        => (number / Pow(10, index)) % 10;
 
     private static int Pow(int x, int y)
         => (int) Math.Pow(x, y);

cb1f5100f3b9dfbb14ffea479c9bb3c384de9e75
Alexander Akhtyrskiy <lex57ukr@gmail.com>
2018-01-15 10:27:25 -0700

    add flatten-array

---
+++ flatten-array/FlattenArray.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+
+
+public static class FlattenArray
+{
+    public static IEnumerable Flatten(IEnumerable input)
+    {
+        var acc = new ArrayList();
+        Flatten(input, acc);
+
+        return acc;
+    }
+
+    private static void Flatten(object item, ArrayList acc)
+    {
+        if (item == null)
+        {
+            return;
+        }
+
+        if (item is IEnumerable collection)
+        {
+            foreach (var x in collection)
+            {
+                Flatten(x, acc);
+            }
+        }
+        else
+        {
+            acc.Add(item);
+        }
+    }
+}

5f45b849f6024c64bbc2b2b1f7e94aee42652128
Alexander Akhtyrskiy <lex57ukr@gmail.com>
2018-01-20 19:17:31 -0700

    implement binary-search-tree

--- binary-search-tree/BinarySearchTree.cs
+++ binary-search-tree/BinarySearchTree.cs
@@ -6,50 +6,102 @@ using System.Collections.Generic;
 public class BinarySearchTree
     : IEnumerable<int>
 {
-    public BinarySearchTree(int value)
-    {
-    }
-
-    public BinarySearchTree(IEnumerable<int> values)
-    {
-    }
-
     public int Value
     {
-        get
-        {
-            throw new NotImplementedException();
-        }
+        get;
     }
 
     public BinarySearchTree Left
     {
-        get
-        {
-            throw new NotImplementedException();
-        }
+        get;
+        private set;
     }
 
     public BinarySearchTree Right
     {
-        get
+        get;
+        private set;
+    }
+
+    public BinarySearchTree(int value)
+        => this.Value = value;
+
+    public BinarySearchTree(IEnumerable<int> values)
+    {
+        using (var enumerator = values.GetEnumerator())
         {
-            throw new NotImplementedException();
+            if (! enumerator.MoveNext())
+            {
+                throw new ArgumentException("The collection is empty.");
+            }
+
+            this.Value = enumerator.Current;
+            while (enumerator.MoveNext())
+            {
+                Add(enumerator.Current);
+            }
         }
     }
 
     public BinarySearchTree Add(int value)
     {
-        throw new NotImplementedException();
+        if (value <= this.Value)
+        {
+            AddLeft(value);
+        }
+        else
+        {
+            AddRight(value);
+        }
+
+        return this;
     }
 
     public IEnumerator<int> GetEnumerator()
     {
-        throw new NotImplementedException();
+        if (this.Left != null)
+        {
+            foreach (var x in this.Left)
+            {
+                yield return x;
+            }
+        }
+
+        yield return this.Value;
+
+        if (this.Right != null)
+        {
+            foreach (var x in this.Right)
+            {
+                yield return x;
+            }
+        }
     }
 
     IEnumerator IEnumerable.GetEnumerator()
+        => GetEnumerator();
+
+    private void AddLeft(int value)
     {
-        throw new NotImplementedException();
+        if (this.Left == null)
+        {
+            this.Left = new BinarySearchTree(value);
+        }
+        else
+        {
+            this.Left.Add(value);
+        }
+    }
+
+    private void AddRight(int value)
+    {
+        if (this.Right == null)
+        {
+            this.Right = new BinarySearchTree(value);
+        }
+        else
+        {
+            this.Right.Add(value);
+        }
     }
 }

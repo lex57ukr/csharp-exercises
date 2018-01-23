ce5f591e981fd02d0a9fbab471a9ec74161bfe54
Alexander Akhtyrskiy <lex57ukr@gmail.com>
2018-01-22 17:07:18 -0700

    add atbash-cipher

---
+++ atbash-cipher/AtbashCipher.cs
@@ -0,0 +1,15 @@
+using System;
+
+
+public static class AtbashCipher
+{
+    public static string Encode(string plainValue)
+    {
+        throw new NotImplementedException();
+    }
+
+    public static string Decode(string encodedValue)
+    {
+        throw new NotImplementedException();
+    }
+}

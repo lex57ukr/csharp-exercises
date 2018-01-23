12a3eef14bed4f2ed248813501bc3d0ff3a48af4
Alexander Akhtyrskiy <lex57ukr@gmail.com>
2018-01-22 18:21:50 -0700

    implement adbash-cipher

--- atbash-cipher/AtbashCipher.cs
+++ atbash-cipher/AtbashCipher.cs
@@ -1,15 +1,46 @@
 using System;
+using System.Linq;
 
 
 public static class AtbashCipher
 {
+    private const int GroupSize = 5;
+    private const int AlphabetSize = 26;
+
     public static string Encode(string plainValue)
     {
-        throw new NotImplementedException();
+        var transcoded = plainValue
+            .Where(char.IsLetterOrDigit)
+            .Select(char.ToLower)
+            .Select(Transcode);
+
+        var groups = transcoded.Select(
+            (x, i) => (Char: x, Index: i)
+        ).GroupBy(
+            x => x.Index / GroupSize,
+            x => x.Char
+        ).Select(
+            x => string.Concat(x)
+        );
+
+        return string.Join(" ", groups);
     }
 
     public static string Decode(string encodedValue)
     {
-        throw new NotImplementedException();
+        var transcoded = encodedValue
+            .Where(char.IsLetterOrDigit)
+            .Select(Transcode);
+
+        return string.Concat(transcoded);
     }
+
+    private static char Transcode(char c)
+        => char.IsLetter(c) ? Encode(c) : c;
+
+    private static char Encode(char c)
+        => (char) (Encode((int) c - (int) 'a') + (int) 'a');
+
+    private static int Encode(int x)
+        => (AlphabetSize - 1) * (x + 1) % AlphabetSize;
 }

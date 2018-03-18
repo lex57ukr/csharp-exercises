2e0e6266e363e30376a0c802408745bbcb662921
Alex Akhtyrskiy <lex57ukr@gmail.com>
2018-03-18 17:55:15 -0600

    add grep

---
+++ grep/Grep.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+
+public static class Grep
+{
+    public static string Find(string pattern, string flags, string[] files)
+    {
+        throw new NotImplementedException();
+    }
+}

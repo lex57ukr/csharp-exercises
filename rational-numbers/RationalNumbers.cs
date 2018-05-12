0952e3ec61f94f4c36d33b6ca6bd51916a094907
Alexander Akhtyrskiy <lex57ukr@gmail.com>
2018-05-11 21:20:03 -0600

    add standard struct methods

--- rational-numbers/RationalNumbers.cs
+++ rational-numbers/RationalNumbers.cs
@@ -11,20 +11,52 @@ public static class RealNumberExtension
 }
 
 public struct RationalNumber
+    : IEquatable<RationalNumber>
 {
+    private int _numerator, _denominator;
+
     public RationalNumber(int numerator, int denominator)
     {
+        _numerator   = numerator;
+        _denominator = denominator;
     }
 
     public RationalNumber Add(RationalNumber r)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return new RationalNumber(
+            _numerator * r._denominator + r._numerator * _denominator,
+            _denominator * r._denominator
+        );
+    }
+
+    public override string ToString()
+    {
+        return _numerator == _denominator
+            ? _numerator.ToString()
+            : $"{_numerator}/{_denominator}";
+    }
+
+    public override int GetHashCode()
+    {
+        var hash = 23;
+
+        unchecked
+        {
+            hash = (hash * 31) + _numerator;
+            hash = (hash * 31) + _denominator;
+        }
+
+        return hash;
+    }
+
+    public bool Equals(RationalNumber other)
+    {
+        return _numerator   == other._numerator
+            && _denominator == other._denominator;
     }
 
     public static RationalNumber operator +(RationalNumber r1, RationalNumber r2)
-    {
-        throw new NotImplementedException("You need to implement this operator.");
-    }
+        => r1.Add(r2);
 
     public RationalNumber Sub(RationalNumber r)
     {
